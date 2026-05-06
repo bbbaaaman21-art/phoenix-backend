dc6a5db77b56d860b45e7a1253ba43b67731ae5b
bbbaaaman21-art <bbbaaaman21@gmail.com>
2026-05-06 14:20:12 +0300

    fix brevo

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,55 +1,52 @@
-const SibApiV3Sdk = require("@getbrevo/brevo");
-
-const client = SibApiV3Sdk.ApiClient.instance;
-
-const apiKey = client.authentications["api-key"];
-apiKey.apiKey = process.env.BREVO_API_KEY;
-
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+const brevo = require("@getbrevo/brevo");
 
 async function sendEmail(to, resetLink) {
 
+  const apiInstance = new brevo.TransactionalEmailsApi();
+
+  apiInstance.setApiKey(
+    brevo.TransactionalEmailsApiApiKeys.apiKey,
+    process.env.BREVO_API_KEY
+  );
+
   try {
 
-    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    await apiInstance.sendTransacEmail({
 
-    sendSmtpEmail.sender = {
-      email: process.env.EMAIL_FROM,
-      name: "Rovix Home"
-    };
+      sender: {
+        email: process.env.EMAIL_FROM,
+        name: "Rovix Home"
+      },
 
-    sendSmtpEmail.to = [{ email: to }];
+      to: [{ email: to }],
 
-    sendSmtpEmail.subject = "إعادة تعيين كلمة المرور";
+      subject: "إعادة تعيين كلمة المرور",
 
-    sendSmtpEmail.htmlContent = `
-      <div style="font-family:Arial; direction:rtl">
-        <h2>إعادة تعيين كلمة المرور</h2>
+      htmlContent: `
+        <div style="font-family:Arial; direction:rtl">
+          <h2>إعادة تعيين كلمة المرور</h2>
 
-        <p>اضغط على الزر لتغيير كلمة المرور:</p>
+          <p>اضغط على الزر لتغيير كلمة المرور:</p>
 
-        <a href="${resetLink}"
-           style="
-             display:inline-block;
-             padding:12px 20px;
-             background:#111;
-             color:#fff;
-             border-radius:8px;
-             text-decoration:none;
-           ">
-           تغيير كلمة المرور
-        </a>
-      </div>
-    `;
-
-    await apiInstance.sendTransacEmail(sendSmtpEmail);
+          <a href="${resetLink}"
+             style="
+               display:inline-block;
+               padding:12px 20px;
+               background:#111;
+               color:#fff;
+               border-radius:8px;
+               text-decoration:none;
+             ">
+             تغيير كلمة المرور
+          </a>
+        </div>
+      `
+    });
 
     console.log("✅ Email sent");
 
   } catch (err) {
-
     console.error("❌ Brevo error:", err.response?.body || err.message);
-
     throw err;
   }
 }
