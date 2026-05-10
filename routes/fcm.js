eb107fc72c22716076ba2fc6b13ce93f17683201
bbbaaaman21-art <bbbaaaman21@gmail.com>
2026-05-10 15:15:15 +0300

    fcm

---
+++ routes/fcm.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+
+let savedToken = null;
+
+router.post("/save-fcm-token", (req, res) => {
+
+  savedToken = req.body.fcmToken;
+
+  console.log("FCM TOKEN SAVED:", savedToken);
+
+  res.json({
+    success: true
+  });
+
+});
+
+router.get("/test-notification", async (req, res) => {
+
+  try {
+
+    const admin = require("../config/firebase");
+
+    await admin.messaging().send({
+      token: savedToken,
+
+      notification: {
+        title: "🛒 طلب جديد",
+        body: "تم استلام أوردر جديد"
+      }
+    });
+
+    res.json({
+      success: true
+    });
+
+  } catch (err) {
+
+    console.log(err);
+
+    res.status(500).json({
+      error: err.message
+    });
+
+  }
+
+});
+
+module.exports = router;
